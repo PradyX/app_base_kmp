9b73b5cfd99e87706f0c877ab3a32274abca8cea
Pradyx <pradyumnbhushan@gmail.com>
2024-09-23 22:59:04 +0530

    android: customisation splash

--- composeApp/src/androidMain/kotlin/com/prady/app/base/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/prady/app/base/kmp/MainActivity.kt
@@ -6,11 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        // use this to hold splash screen
+       /* var isChecking = true
+        lifecycleScope.launch {
+            delay(2000)
+            isChecking = false
+        }*/
+        installSplashScreen().apply {
+            /*setKeepOnScreenCondition{
+                isChecking // check condition
+            }*/
+        }
         setContent {
             App()
         }
